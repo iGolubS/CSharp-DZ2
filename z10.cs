3ff1adfe6a374c3a834813ad09da00d6fa997ee6
iGolubS <109303592+iGolubS@users.noreply.github.com>
2022-08-10 21:33:10 +0300

    Add files via upload

---
+++ z10.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Ведите любое трехзначное число, я выведу вам вторую цифру заданного числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>0)
+{
+    if (num>99 && num<1000)
+    {
+        System.Console.WriteLine((num/10)%10);
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели не трехзначное число!");
+    }
+}
+if (num<0)
+{
+    if (num<-99 && num>-1000)
+    {
+        System.Console.WriteLine(-(num/10)%10);
+    }
+    else
+    {
+        System.Console.WriteLine("Вы ввели не трехзначное число!");
+    }
+}
